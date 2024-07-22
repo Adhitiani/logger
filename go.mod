e2db997e8d398545da50f68f622d52f25c0b2dd9
adhitiani.suzanthi <adhitiani@gmail.com>
2024-07-22 14:35:55 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Adhitiani/logger
+
+go 1.22.4
---
+++ logger.go
@@ -0,0 +1,15 @@
+package logger
+
+import "fmt"
+
+func Info(data any) {
+    fmt.Println("Log info: ", data)
+}
+
+func Error(data any) {
+    fmt.Println("Log error: ", data)
+}
+
+func Warning(data any) {
+    fmt.Println("Log warning: ", data)
+}
